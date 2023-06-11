dff432946e9aca461ad7c01b7480bd35a6cb50fa
Raghvendra <126984826+Raghav1234567898@users.noreply.github.com>
2023-06-11 20:59:51 +0530

    Add files via upload

---
+++ public/javascripts/location.js
@@ -0,0 +1,16 @@
+exports.location = function (location){ 
+    var latitude;
+    var longitude;
+    const successCallback = (position) => {
+    console.log(position);
+    latitude = position.coords.latitude
+    longitude = position.coords.longitude
+    };
+    
+    const errorCallback = (error) => {
+    console.log(error);
+    };
+    
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    
+    }
